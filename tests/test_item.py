bcd3396446b080069d6836c49409fb9afe32fb6f
Nikita Kislykh <kislykhnik@gmail.com>
2023-05-22 20:31:12 +0500

    дописал новые тесты к новым методам

--- tests/test_item.py
+++ tests/test_item.py
@@ -24,3 +24,20 @@ def test_all_items():
         item1 = Item("item1", 10, 5)
         item2 = Item("item2", 20, 3)
         assert Item.all == [item1, item2]
+
+
+def test_name_setter(item):
+    item.name = "Эппл"
+    assert item.name == "Эппл"
+
+
+def test_long_name_setter(item):
+    """Тестирует сеттер  self.__name."""
+    with pytest.raises(ValueError):
+        item.name = 'Самсунгусоптимапроплюс'
+
+
+def test_string_to_number(item):
+    """Тестирует метод string_to_number."""
+    assert item.string_to_number("10000") == 10000
+    assert item.string_to_number("2.034") == 2
