7c497f51a3d164829fd746ac859b90fb4c6271a0
Nikita Kislykh <kislykhnik@gmail.com>
2023-05-22 20:30:29 +0500

    добавил новые методы в класс

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all = self.all.append(self)
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -24,7 +28,7 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        total_price = self.price*self.quantity
+        total_price = self.price * self.quantity
         return total_price
 
     def apply_discount(self) -> float:
@@ -32,3 +36,29 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
+
+    @property
+    def name(self) -> str:
+        """
+        Геттер для названия товара.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, str_line: str) -> None:
+        """Сеттер для названия товара."""
+        if len(str_line) > 10:
+            raise ValueError("Название должно быть не более 10 символов.")
+        self.__name = str_line
+
+    @classmethod
+    def instantiate_from_csv(cls, PATH_TO_CSV='../src/items.csv'):
+        with open(PATH_TO_CSV, encoding='windows-1251') as file:
+            reader = csv.DictReader(file)
+            for data in reader:
+                cls.all.append((data['name'], float(data['price']), int(data['quantity'])))
+
+    @staticmethod
+    def string_to_number(number: str) -> int:
+        """Преобразование строки в число."""
+        return int(number.split(".")[0])
