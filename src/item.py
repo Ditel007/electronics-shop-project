ad9b81d3c3cfa23bf50d4aa514a19a322a2436dd
Nikita Kislykh <kislykhnik@gmail.com>
2023-05-25 01:25:39 +0500

    добавил классу методы __repr__ & __str__

--- src/item.py
+++ src/item.py
@@ -22,6 +22,15 @@ class Item:
 
         Item.all.append(self)
 
+        # assert repr(item1) == "Item('Смартфон', 10000, 20)"
+        # assert str(item1) == 'Смартфон'
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.__name}"
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
